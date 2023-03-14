6407c554f8ece28d69f569e070d539c53052224e
shreyas.kv <shreyas.kv@accord-soft.com>
2023-03-14 18:35:33 +0530

    Initial commit

---
+++ app/src/main/java/com/androiddevs/newsapp/db/ArticleDatabase.kt
@@ -0,0 +1,30 @@
+package com.androiddevs.newsapp.db
+
+import android.content.Context
+import androidx.room.*
+import com.androiddevs.newsapp.models.Article
+
+@Database(
+    entities = [Article::class],
+    version = 1
+)
+
+@TypeConverters(Converters::class)
+abstract class ArticleDatabase : RoomDatabase() {
+
+    abstract fun getArticleDao(): ArticleDao
+
+    companion object{
+
+        @Volatile //so that other threads can immediately see when it changes
+        private var instance: ArticleDatabase?=null
+        private val LOCK = Any()
+
+        operator fun invoke(context:Context)= instance?: synchronized(LOCK){
+            instance?:createDatabase(context).also{ instance =  it}
+        }
+
+        private fun createDatabase(context: Context) =
+            Room.databaseBuilder(context.applicationContext, ArticleDatabase::class.java,"article_db.db").build()
+    }
+}
---
+++ app/src/main/java/com/androiddevs/newsapp/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.androiddevs.newsapp.util
+
+class Constants {
+    //Like static variables in java
+    companion object{
+        const val API_KEY = "0861583e005c436595f432fcbe581d99"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
